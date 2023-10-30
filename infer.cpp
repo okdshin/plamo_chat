df848656052ba288e9dd1fcdfd1755dcf7214a5e
okada <kokuzen@gmail.com>
2023-10-30 23:40:53 +0900

    use kv_cache

--- infer.cpp
+++ infer.cpp
@@ -1,3 +1,4 @@
+#include <iostream>
 #include <cassert>
 #include <pybind11/numpy.h>
 #include <pybind11/pybind11.h>
@@ -34,18 +35,29 @@ public:
 
   py::array_t<float> calc_next_token_logits(py::array_t<int> const &input_ids,
                                             size_t vocab_size) {
-    py::print("input_ids = ", input_ids);
     assert(input_ids.shape(0) == 1); // batch_size must be 1
     llama_batch batch = llama_batch_init(2048, 0); // TODO
-    batch.n_tokens = input_ids.shape(1);
-    for (size_t i = 0; i < batch.n_tokens; ++i) {
-      batch.token[i] = *input_ids.data(0, i); // TODO
-      batch.pos[i] = i;
-      batch.seq_id[i] = 0;
-      batch.logits[i] = false;
+    std::cout << "hoge" << std::endl;
+    if (is_first(input_ids)) {
+      py::print("FIRST, input_ids = ", input_ids);
+      llama_kv_cache_tokens_rm(ctx_, -1, -1);
+      batch.n_tokens = input_ids.shape(1);
+      for (size_t i = 0; i < batch.n_tokens; ++i) {
+        batch.token[i] = *input_ids.data(0, i);
+        batch.pos[i] = i;
+        batch.seq_id[i] = 0;
+        batch.logits[i] = false;
+      }
+      batch.logits[batch.n_tokens - 1] = true;
+    } else {
+      py::print("input_ids = ", input_ids);
+      batch.token[0] = *input_ids.data(0, input_ids.shape(1) - 1);
+      batch.pos[0] = input_ids.shape(1) - 1;
+      batch.seq_id[0] = 0;
+      batch.logits[0] = true;
+      batch.n_tokens = 1;
     }
-    batch.logits[batch.n_tokens - 1] = true;
-    //if (auto result = llama_decode(ctx_, batch); result != 0) {
+    // if (auto result = llama_decode(ctx_, batch); result != 0) {
     if (auto result = llama_decode(ctx_, batch); result < 0) {
       throw std::runtime_error("llama_decode failed " + std::to_string(result));
     }
@@ -54,17 +66,37 @@ public:
         std::vector<size_t>{static_cast<size_t>(input_ids.shape(0)), 1u,
                             vocab_size},
         logits_data);
-    py::print("logits = ", logits);
-    /*
-    py::array_t<float> logits(std::vector<size_t>{
-        static_cast<size_t>(input_ids.shape(0)), 1u, vocab_size});
-    */
+    //py::print("logits = ", logits);
     return logits;
   }
 
 private:
   plamo_cpp_model(llama_model *model, llama_context *ctx)
       : model_(model), ctx_(ctx) {}
+
+  bool is_first(py::array_t<int> const &input_ids) {
+      static py::array_t<int> input_ids_before_backup = py::array_t<int>();
+      py::array_t<int> input_ids_before = input_ids_before_backup;
+      input_ids_before_backup = input_ids;
+      if(input_ids_before.ndim() != input_ids.ndim()) {
+          return true;
+      }
+      if(input_ids_before.shape(0) != input_ids.shape(0)) {
+          return true;
+      }
+      if(input_ids_before.shape(1) > input_ids.shape(1)) {
+          return true;
+      }
+      for(size_t i = 0; i < input_ids_before.shape(0); ++i) {
+          for(size_t j = 0; j < input_ids_before.shape(1); ++j) {
+              if(*input_ids_before.data(i, j) != *input_ids.data(i, j)) {
+                  return true;
+              }
+          }
+      }
+      return false;
+  }
+
   llama_model *model_;
   llama_context *ctx_;
 };
